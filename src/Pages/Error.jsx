9eeecf560448f96ffccbcc5cc7b1798cf81abba9
devadigasuhan <suhandevadiga01@gmail.com>
2025-03-01 11:59:17 +0530

    i-did-changes-in-alt-text

--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Error() {
   return (
   <div>
-    <img src="111.jpg" alt="" />
+    <img src="111.jpg" alt="hello git new branch 01-03-2025-s" />
 404 page not found
   </div>
   );
